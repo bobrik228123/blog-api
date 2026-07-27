5d62fabe8930e10f5c318f388cd275dab88c080e
bobrik228123 <nazar5680official2@gmail.com>
2026-07-27 15:54:12 -0400

    Add Post model and relationships

---
+++ app/models/post.py
@@ -0,0 +1,15 @@
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
+from app.database import Base
+
+
+class Post(Base):
+    __tablename__ = "posts"
+    id = Column(Integer, primary_key=True)
+    title = Column(String,nullable=False)
+    content = Column(String,nullable=False)
+    user_id = Column(Integer, ForeignKey("users.id"))
+
+
+    owner = relationship("User", back_populates="posts")
+
--- app/models/user.py
+++ app/models/user.py
@@ -1,5 +1,6 @@
 from sqlalchemy import Column, Integer, String
 from app.database import Base
+from sqlalchemy.orm import relationship
 
 
 class User(Base):
@@ -7,4 +8,6 @@ class User(Base):
     id = Column(Integer, primary_key=True, index=True)
     email = Column(String, unique=True, nullable=False)
     password_hash = Column(String, nullable=False)
+
+    posts = relationship("Post", back_populates="owner")
 
